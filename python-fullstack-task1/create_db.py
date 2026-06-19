df9abcf5cb14ee6bb799a60191cb9677d37c176b
SHAIK NAYAB RASOOL <2501600077@kluniversity.in>
2026-06-19 21:36:29 +0530

    Add files via upload

---
+++ python-fullstack-task1/create_db.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+
+conn.execute('''
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT
+)
+''')
+
+conn.commit()
+conn.close()
+
+print("Database Created Successfully")
